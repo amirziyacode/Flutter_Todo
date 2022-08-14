965352bf884114ee97c69437040eec09dfc46ae4
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2022-08-14 14:12:08 +0430

    new item

---
+++ lib/data/advice.dart
@@ -0,0 +1,29 @@
+class Advices {
+  String name;
+  String description;
+
+  Advices({required this.name, required this.description});
+}
+
+List<Advices> advices = [
+  Advices(
+      name: 'René Descartes',
+      description:
+          "It is only intelligence that is divided equally between people because everybody thinks he/she is enough wise"),
+  Advices(
+      name: "Dr. Ali Shariati",
+      description:
+          "My father chose my name ,\n and my last name was \n chosen by my ancestors \n That’s enough,\n I myself choose my way"),
+  Advices(
+      name: "Ali Shariati",
+      description:
+          "I am surprised by the people who they themselves live under the lash of injusticeand cry for “Husayn” who lived free"),
+  Advices(
+      name: "Great Orod",
+      description:
+          "The country which forgets its own legendstries to satisfy itself by other countries’ \nlegendsChildren of such a dynasty are open and vulnerable"),
+  Advices(
+      name: "Howard Baskerville",
+      description:
+          "The only difference between me and these people is my place of birth and this is not a big difference"),
+];
