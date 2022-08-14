f349182f4558ebb47794869472692a3ee32d917f
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2022-08-14 14:55:05 +0430

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/pages/Drawerhiden/hidendrawer.dart';
+import 'package:flutter_todo/db/noted.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'data/shared/Task_saved.dart';
 import 'data/thems.dart';
 
+late Box box;
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  await TaskerPreference.init(); // for initial SharedPerfomance .. 
-  runApp(const MyApp());
+  await Hive.initFlutter();
+  Hive.registerAdapter(NotesAdapter());
+  box = await Hive.openBox<Notes>("notes");
+  await TaskerPreference.init(); // for initial SharedPerfomance ..
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget{
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
@@ -30,4 +34,3 @@ class MyApp extends StatelessWidget {
         );
       });
 }
-
